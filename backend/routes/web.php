fe0ea69ab927613fbe7597402ea4783b4851ad5b
bone1nis <bone1nis@gmail.com>
2025-05-22 18:48:39 +0300

    feat(backend, docker): add Redis extension to Dockerfile and configure Redis on backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/redis-test', function () {
+    \Illuminate\Support\Facades\Cache::put('foo', 'bar', 10);
+    return \Illuminate\Support\Facades\Cache::get('foo');
+});
